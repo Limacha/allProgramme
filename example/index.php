1135b979893efb135b5de88c1cf62acae54ac271
Limacha <257nelson@gmail.com>
2023-10-17 10:40:35 +0200

    modification du fichier
    ajout d'un fichier example et creation du nouveau fichier de l'exercise

---
+++ example/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example : Ponchaut Nicolas</title>
+    <link rel="icon" href="./image/iconSiteWeb.png">
+    <link rel="stylesheet" href="./css/flex.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
