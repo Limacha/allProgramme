aaf188dd121259bde3e7fc0d125b91334ee9995a
Limacha <257nelson@gmail.com>
2023-11-07 11:27:20 +0100

    new: first_function
    creation du dossier permetant de faire l'exercise firstvfunction en php

---
+++ php/first_function/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$nd = 5;
+$nt  = 8;
+$nbr1  = 21;
+$nbr2  = 15;
+require_once "function.php";
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first function : Ponchaut Nicolas</title>
+    <link rel="icon" href="./image/iconSiteWeb.png">
+    <link rel="stylesheet" href="./css/flex.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
